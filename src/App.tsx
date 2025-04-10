37f3e1bf52895311179759845ad32446d89e43f5
mterrera29 <matiasterrera29@gmail.com>
2025-04-09 23:47:06 -0300

    nivel 3

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Memotest3 from './components/Memotest3/Memotest3';
 import Memotest4 from './components/Memotest4/Memotest4';
 import Memotest5 from './components/Memotest5/Memotest5';
-import { PuntosProvider } from './context/PuntosContext';
 import MemoMain from './components/MemoMain';
 
 function App() {
   return (
-    <PuntosProvider>
-      <BrowserRouter>
-        <div>
-          <Routes>
-            <Route element={<MemoMain />} path='/' />
-            <Route element={<Memotest3 />} path='/nivel1' />
-            <Route element={<Memotest4 />} path='/nivel2' />
-            <Route element={<Memotest5 />} path='/nivel3' />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </PuntosProvider>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route element={<MemoMain />} path='/' />
+          <Route element={<Memotest3 />} path='/nivel1' />
+          <Route element={<Memotest4 />} path='/nivel2' />
+          <Route element={<Memotest5 />} path='/nivel3' />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
